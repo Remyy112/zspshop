14bc44638974c7c6e7558bf55c5f0e6693d3b0cc
Remyy112 <remigiuszpawlowski05@gmail.com>
2022-11-09 12:48:57 +0100

    zapis1

--- zspshop.php
+++ zspshop.php
@@ -5,8 +5,13 @@
     <link rel="stylesheet" href="stylezspshop.css">
 </head>
 <body>
-    
-    
-    
+    <div class="header">
+        ZSP shop
+    </div>
+    <div class="body1">
+        <br>
+        <h1>Logowanie</h1>
+        <input type="text">
+    </div>
     
 </body>
